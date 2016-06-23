538df550e59db666d96d36aa3078a65d643095cf
hashmat MH <hashmat00@aol.com>
2016-06-23 20:20:14 +0000

    added routes on app.js

--- IntroToExpress/firstApp/app.js
+++ IntroToExpress/firstApp/app.js
@@ -2,8 +2,20 @@ var express = require("express");
 
 var app = express();
 
+
+
 app.get('/', function(req, res){
     res.send('hi there');
+});
+
+
+app.get('/r/:category', function(req, res) {
+     var categories = req.params.category;
+    res.send("You are in " + categories.toUpperCase() + " " + " Category");
+});
+
+app.get('/r/:category/comments/:id/:title', function(req, res) {
+    res.send("welcome to comments id title page");
 });
 
 app.get('/bye', function(req, res){
@@ -14,6 +26,15 @@ app.get('/bye', function(req, res){
 app.get('/dog', function(req, res) {
     res.send('MEOW');
 });
+
+app.get('*', function(req, res) {
+    res.send("404 page not found");
+});
+
+
+
+
+
 
 
 app.listen(process.env.PORT, process.env.IP, function(){
