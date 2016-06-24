8ec3462cae28ea838d99d94f9f7e62f46f36c313
hashmat MH <hashmat00@aol.com>
2016-06-24 03:01:24 +0000

    added yahoo weather api for hawai

--- IntroToExpress/API/request.js
+++ IntroToExpress/API/request.js
@@ -10,14 +10,10 @@ var request = require('request');
 //         }
 //     }
 // });
-
+console.log("Sunset time in HAWAI ..")
 request('https://query.yahooapis.com/v1/public/yql?q=select%20astronomy.sunset%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22maui%2C%20hi%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys', function(error, response, body){
-    if(error){
-        console.log("somethign wrong");
-        console.log(error);
-    }else{
-        if(response.statusCode == 200){
-            console.log(body);
-        }
+    if(!error && response.statusCode == 200){
+        var data = JSON.parse(body);
+        console.log("==  " + data['query']['results']['channel']['astronomy']['sunset']+ "  ==  ");
     }
 });
