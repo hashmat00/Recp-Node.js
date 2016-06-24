5a4af935e84104b186190558cae3033d4da02aad
hashmat MH <hashmat00@aol.com>
2016-06-24 00:00:57 +0000

    added partials, header, footer

--- IntroToExpress/IntroEjs/app.js
+++ IntroToExpress/IntroEjs/app.js
@@ -1,16 +1,32 @@
 var express = require("express");
 var app = express();
 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+
+
 
 app.get('/', function(req, res){
-    res.render('home.ejs');
+    res.render('home');
 });
 
 
 app.get('/love/:message', function(req, res){
     var message = req.params.message;
-    res.render('love.ejs', {messages: message});
-})
+    res.render('love', {messages: message});
+});
+
+
+app.get('/post', function(req, res) {
+    var posts = [
+        {title: "My first app", author: "hashmat"},
+        {title: "Seasoanl journal", author: "James"},
+        {title: "Welcome back", author: "John smtih"}
+    ];
+    
+    res.render('posts', {posts: posts});
+});
 
 
 
