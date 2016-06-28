a7d483c500569404a2e0390f1d328702e071d54c
hashmat MH <hashmat00@aol.com>
2016-06-28 21:59:38 +0000

    added new blog route, added form and styled it

--- CampApp/RESTfulRouting/app.js
+++ CampApp/RESTfulRouting/app.js
@@ -50,8 +50,33 @@ app.get('/blogs', function(req, res){
 
 
 
+app.get('/blogs/new', function(req, res) {
+   res.render('new'); 
+});
 
 
+
+app.post('/blogs', function(req, res) {
+   //create blog
+    Blog.create(req.body.blog, function(err, newBlog){
+        if(err){
+            res.render('new');
+        }else{
+            //redirect to index
+            res.redirect('/blogs');
+        }
+        
+    })
+   
+});
+
+
+
+
+
+//===========================
+//== start SERVER 
+//===========================
 
 app.listen(process.env.PORT, process.env.IP, function(req, res){
     console.log("Server has started");
