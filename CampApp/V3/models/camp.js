d8e06a40bd78f42c3d8fe17f4992b78aaa1f8a25
hashmat MH <hashmat00@aol.com>
2016-06-30 21:47:41 +0000

    added v3

---
+++ CampApp/V3/models/camp.js
@@ -0,0 +1,12 @@
+var mongoose  = require("mongoose");
+
+
+
+
+var campSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+});
+
+module.exports = mongoose.model("camp", campSchema);
