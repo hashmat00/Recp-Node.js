7d1918de6aad46ec83b65a0d963fabe8c7e7c47c
hashmat MH <hashmat00@aol.com>
2016-07-04 17:57:22 +0000

    added user to comment author schema

---
+++ CampApp/V8/routes/camp.js
@@ -0,0 +1,82 @@
+var express = require("express");
+var router = express.Router();
+
+
+var Camp = require("../models/camp")
+
+
+
+
+
+router.get('/', function(req, res) {
+    // res.render('camp', {camp: camp});
+    
+    //retrieve all camps from DB 
+        Camp.find({}, function(err, allCamps){
+            if(err){
+               console.log("Something is wrong");
+                console.log(err);
+            }else{
+                
+                res.render("index", {camp: allCamps});
+            }
+        });
+        
+        
+});
+
+
+
+
+router.post('/', function(req, res){
+   var name = req.body.name;
+    var image = req.body.image;
+    var description = req.body.description;
+    var NewCamp = {name: name, image: image, description: description};
+    
+    //create new camp
+    Camp.create(NewCamp, function(err, Createcamp){
+        if(err){
+            console.log(err);
+        }else{
+            //redirect to camp listing page after successfully adding a camp
+            res.redirect('/camp');
+        }
+    });
+});
+
+
+
+router.get('/new', isLoggedIn, function(req, res) {
+    res.render('new.ejs');
+});
+
+
+router.get('/:id', function(req, res) {
+    // res.send("This is the View page of camp");
+    
+    Camp.findById(req.params.id).populate("comments").exec(function(err, foundCamp){
+        if(err){
+            console.log(err);
+        }else{
+            res.render('show', {camp: foundCamp});
+        }
+    });
+});
+
+
+
+
+
+// isLoggedIn
+function isLoggedIn(req, res, next){
+   if(req.isAuthenticated()){
+       return next();
+   }
+   
+   res.redirect('/login');
+}
+
+
+
+module.exports = router;
