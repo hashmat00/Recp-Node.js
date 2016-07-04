73cfef52eb736a08722b6c0269240ceb705bd882
hashmat MH <hashmat00@aol.com>
2016-07-04 18:21:05 +0000

    added author to camp, where it shows who added a camp, displayed authro name on every camps

--- CampApp/V8/models/camp.js
+++ CampApp/V8/models/camp.js
@@ -7,6 +7,15 @@ var campSchema = new mongoose.Schema({
    name: String,
    image: String,
    description: String,
+   author: {
+         id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+         },
+         
+         username: String
+   },
+   
    comments: [
       {
          type: mongoose.Schema.Types.ObjectId,
--- CampApp/V8/routes/camp.js
+++ CampApp/V8/routes/camp.js
@@ -32,7 +32,11 @@ router.post('/', function(req, res){
    var name = req.body.name;
     var image = req.body.image;
     var description = req.body.description;
-    var NewCamp = {name: name, image: image, description: description};
+    var author = {
+        id: req.user._id,
+        username: req.user.username
+    }
+    var NewCamp = {name: name, image: image, description: description, author: author};
     
     //create new camp
     Camp.create(NewCamp, function(err, Createcamp){
@@ -40,6 +44,7 @@ router.post('/', function(req, res){
             console.log(err);
         }else{
             //redirect to camp listing page after successfully adding a camp
+         
             res.redirect('/camp');
         }
     });
